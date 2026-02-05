1372c79e46abdceaa88fddd0f60328ae2a7c271f
Patcharida07 <game22403030@gmail.com>
2026-02-05 15:11:12 +0900

    import css

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./Login.css";
+// import styles from "./Login.css";
+import "./Login.css";
 
 const Login = () => {
     const [userename, setUsername] = useState("");
